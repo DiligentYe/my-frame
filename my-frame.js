28d7489eb724ec337f84947540aa7bb5667ac6de
yyp <yyp@yypdeMacBook-Pro.local>
2017-03-04 15:17:15 +0800

    查询字符串处理函数 queryString

--- my-frame.js
+++ my-frame.js
@@ -4,5 +4,27 @@
         // '  hello world  '
         return str.replace(/(^\s*)|(\s*$)/g, '');
     };
+
+    // 查询字符串处理函数 queryString
+    myFrame.queryString = function(str) {
+        var qs = str.substring(1);
+
+        var args = {};
+
+        var items = qs.length ? qs.split('&') : [];
+
+        var key = '',
+            value = '';
+
+        for (var i = 0, len = items.length; i < len; ++i) {
+            var item = items[i].split('=');
+            key = item[0];
+            value = item[1];
+
+            args[key] = value;
+        }
+
+        return args;
+    };
     window.myFrame = myFrame;
 })(window.myFrame || {});
